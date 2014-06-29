50a34fc86f0a99d421a5d0f947cb891f0fbd371c
Rutger Rauws <rutger_rauws@hotmail.com>
2014-06-29 03:05:48 +0200

    Wrote basic scrape method to parse all images on one steam screenshot webpage in Scraper.java I decided to use 'jsoup' to parse HTML.

---
+++ Steam-Screenshot-Downloader/src/steam/screenshot/downloader/Scraper.java
@@ -0,0 +1,25 @@
+package steam.screenshot.downloader;
+
+import java.io.IOException;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+/**
+ *
+ * @author Rutger Rauws
+ */
+public class Scraper
+{
+    public static void downloadScreenshots(String steamID, String path) throws IOException
+    {
+        Document document = Jsoup.connect("http://steamcommunity.com/id/" + steamID + "/screenshots/?p=1&sort=newestfirst&browsefilter=myfiles&view=grid&privacy=14#scrollTop=0").get();
+        Elements images = document.select(".imgWallItem img");
+        
+        for(int fileNumber = 1; fileNumber <= images.size(); fileNumber++)        
+        {
+            String URI = images.eq(fileNumber).attr("src").replaceFirst("\\d+x\\d+\\.resizedimage", "");
+            Data.writeImageFromUrl(URI, path, fileNumber);
+        }
+    }
+}
