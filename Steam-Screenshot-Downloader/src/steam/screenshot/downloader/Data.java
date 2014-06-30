5f35c6698c6f8850ca4fb3a44f3b0348d5766df7
Rutger Rauws <rutger_rauws@hotmail.com>
2014-06-30 18:09:37 +0200

    Now using Threads!

--- Steam-Screenshot-Downloader/src/steam/screenshot/downloader/Data.java
+++ Steam-Screenshot-Downloader/src/steam/screenshot/downloader/Data.java
@@ -10,14 +10,42 @@ import javax.imageio.ImageIO;
  *
  * @author Rutger Rauws
  */
+class DataRunnable implements Runnable
+{    
+    private final String URI;
+    private final String path;
+    private final int fileNumber;
+    
+    @Override
+    public void run()
+    {
+        try // I wish I could just throw the exception back to Scraper.java
+        {
+            URL url = new URL(URI);
+            BufferedImage image = ImageIO.read(url);
+            File outputFile = new File(path + "\\" +fileNumber + ".jpg");
+            
+            ImageIO.write(image, "jpg", outputFile);
+        }
+        catch (IOException e)
+        {
+            // What to do..?
+        }
+    }
+    
+    public DataRunnable(String URI, String path, int fileNumber)
+    {
+        this.URI = URI;
+        this.path = path;
+        this.fileNumber = fileNumber;
+    }
+}
+
 public class Data
 {
-    public static void writeImageFromUrl(String URI, String path, int fileNumber) throws IOException
+    public static void writeImageFromUrl(String URI, String path, int fileNumber)
     {
-        URL url = new URL(URI);
-        BufferedImage image = ImageIO.read(url);
-        File outputFile = new File(path + "\\" +fileNumber + ".jpg");
-        
-        ImageIO.write(image, "jpg", outputFile);
+        DataRunnable runnable = new DataRunnable(URI, path, fileNumber);
+        new Thread(runnable).start();
     }
 }
